29bb78bdf6616f4d0bab59e97c1158a6a785df6d
wuyinlei <wuyinlei19931118@hotmail.com>
2018-01-17 21:21:09 +0800

    模块化学习,动态控制library和application的转换

---
+++ app/src/main/java/ruolan/com/kotlin/ui/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package ruolan.com.kotlin.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import ruolan.com.kotlin.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ usercenter/src/main/java/com/ruolan/user/ui/activity/RegisterActivity.kt
@@ -0,0 +1,17 @@
+package com.ruolan.user.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.ruolan.user.R
+
+/**
+ * Created by wuyinlei on 2018/1/17.
+ */
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+
+}
