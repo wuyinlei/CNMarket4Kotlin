e23c583c6c727e8922391165dac83a6006bd9a8d
wuyinlei <wuyinlei19931118@hotmail.com>
2018-02-04 21:36:43 +0800

    购物车的相关逻辑

--- goodscenter/src/main/java/ruolan/com/goodscenter/ui/fragment/CartFragment.kt
+++ goodscenter/src/main/java/ruolan/com/goodscenter/ui/fragment/CartFragment.kt
@@ -6,13 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.alibaba.android.arouter.launcher.ARouter
+import com.eightbitlab.rxbus.Bus
+import com.eightbitlab.rxbus.registerInBus
 import com.kennyc.view.MultiStateView
+import com.kotlin.base.utils.YuanFenConverter
 import com.ruolan.factory.router.RouterPath
 import kotlinx.android.synthetic.main.fragment_cart.*
+import org.jetbrains.anko.support.v4.toast
+import ruolan.com.baselibrary.ext.onClick
 import ruolan.com.baselibrary.ext.setVisible
 import ruolan.com.baselibrary.ui.fragment.BaseMvpFragment
 import ruolan.com.goodscenter.R
 import ruolan.com.goodscenter.data.protocol.CartGoods
+import ruolan.com.goodscenter.event.CartAllCheckedEvent
+import ruolan.com.goodscenter.event.UpdateTotalPriceEvent
 import ruolan.com.goodscenter.injection.component.DaggerCartComponent
 import ruolan.com.goodscenter.injection.module.CartModule
 import ruolan.com.goodscenter.presenter.CartPresenter
@@ -26,9 +33,11 @@ import ruolan.com.goodscenter.ui.adapter.CartGoodsAdapter
  *
  * @function
  */
-class CartFragment : BaseMvpFragment<CartPresenter>(), CartView {
+class CartFragment : BaseMvpFragment<CartPresenter>(), CartView, View.OnClickListener {
 
-    private lateinit var mCartGoodsAdapter :CartGoodsAdapter
+
+    private lateinit var mCartGoodsAdapter: CartGoodsAdapter
+    private var mTotalPrice: Long = 0
 
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -45,7 +54,44 @@ class CartFragment : BaseMvpFragment<CartPresenter>(), CartView {
 
     private fun initObservable() {
 
+        Bus.observe<CartAllCheckedEvent>().subscribe { t: CartAllCheckedEvent ->
+            run {
+                mAllCheckedCb.isChecked = t.isAllChecked
+                updateTotalPrice()
+            }
+        }
+                .registerInBus(this)
 
+        Bus.observe<UpdateTotalPriceEvent>().subscribe {
+            updateTotalPrice()
+        }
+                .registerInBus(this)
+    }
+
+    /*
+      刷新是否为编辑状态
+   */
+    private fun refreshEditStatus() {
+        val isEditStatus = getString(R.string.common_edit) == mHeaderBar.getRightText()
+        mTotalPriceTv.setVisible(isEditStatus.not())
+        mSettleAccountsBtn.setVisible(isEditStatus.not())
+        mDeleteBtn.setVisible(isEditStatus)
+
+        mHeaderBar.getRightView().text = if (isEditStatus) getString(R.string.common_complete) else getString(R.string.common_edit)
+
+
+    }
+
+    /*
+      更新总价
+   */
+    private fun updateTotalPrice() {
+        mTotalPrice = mCartGoodsAdapter.dataList
+                .filter { it.isSelected }
+                .map { it.goodsCount * it.goodsPrice }
+                .sum()
+
+        mTotalPriceTv.text = "合计:${YuanFenConverter.changeF2YWithUnit(mTotalPrice)}"
     }
 
     override fun onStart() {
@@ -55,13 +101,59 @@ class CartFragment : BaseMvpFragment<CartPresenter>(), CartView {
 
     private fun initView() {
         mCartGoodsRv.layoutManager = LinearLayoutManager(activity)
+        mAllCheckedCb.onClick(this)
+        mHeaderBar.getRightView().onClick {
+            refreshEditStatus()
+        }
 
+        //删除按钮事件
+        mDeleteBtn.onClick {
+            val cartIdList: MutableList<Int> = arrayListOf()
+            mCartGoodsAdapter.dataList.filter { it.isSelected }
+                    .mapTo(cartIdList) { it.id }
+            if (cartIdList.size == 0) {
+                toast("请选择需要删除的数据")
+            }else {
+                mPresenter.delCartList(cartIdList)
+            }
+        }
+
+        //结算按钮事件
+        mSettleAccountsBtn.onClick {
+            val cartGoodsList: MutableList<CartGoods> = arrayListOf()
+            mCartGoodsAdapter.dataList.filter { it.isSelected }
+                    .mapTo(cartGoodsList){it}
+            if (cartGoodsList.size == 0) {
+                toast("请选择需要提交的数据")
+            }else {
+                mPresenter.submitCartList(cartGoodsList,mTotalPrice)
+            }
+        }
 
     }
 
     private fun initData() {
         mMultiStateView.viewState = MultiStateView.VIEW_STATE_LOADING
         mPresenter.getCartList()
+
+    }
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.mAllCheckedCb -> {
+                if (mAllCheckedCb.isChecked) {
+                    for (cart in mCartGoodsAdapter.dataList) {
+                        cart.isSelected = true
+                    }
+                    mCartGoodsAdapter.notifyDataSetChanged()
+                } else{
+                    for (cart in mCartGoodsAdapter.dataList) {
+                        cart.isSelected = false
+                    }
+                    mCartGoodsAdapter.notifyDataSetChanged()
+                }
+            }
+        }
 
     }
 
@@ -79,14 +171,14 @@ class CartFragment : BaseMvpFragment<CartPresenter>(), CartView {
             mMultiStateView.viewState = MultiStateView.VIEW_STATE_CONTENT
             mCartGoodsAdapter.setData(result)
             mCartGoodsRv.adapter = mCartGoodsAdapter
-        } else{
+        } else {
             mMultiStateView.viewState = MultiStateView.VIEW_STATE_EMPTY
         }
 
     }
 
     override fun onDeleteCartListResult(result: Boolean) {
-
+        refreshEditStatus()
     }
 
     override fun onSubmitCartListResult(result: Int) {
